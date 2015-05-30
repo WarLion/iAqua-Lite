8d303804bafdc84f75df2ebf844f878130aa1e9c
Miguel Martinez <slverwolf@gmail.com>
2015-05-30 11:54:08 -0500

    Revert "Fix Fade1 Derp.."
    This reverts commit 828e5a765ea099ec84c8bfbc29cc73fd8e0d9840.

--- iAquaLite/iAquaLite.ino
+++ iAquaLite/iAquaLite.ino
@@ -1469,6 +1469,8 @@ void drawScreen()
     if (editingActive==false) {
       fadeT.onHour=fade1.onHour;
       fadeT.onMinute=fade1.onMinute;
+      fadeT.durationHours=fade1.offHour;   //AH bad code
+      fadeT.durationMinutes=fade1.offMinute;  //AH bad code
     }
 
     drawFadeSchedule(fadeT.onHour, fadeT.onMinute, fadeT.durationHours, fadeT.durationMinutes);
